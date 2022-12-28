d359764a0acdd538e77bf42a0819509d3bc8cc3f
Janaka Vithanage <jkvithana@gmail.com>
2022-12-28 23:21:13 +1100

    Basic CRUD operations implemented.

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,49 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path, notice: "Category was succesfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path, notice: "Category was succesfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path, status: :see_other, alert: "Category was deleted successfully."
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :transactions
+
+  validates :name, presence: true, uniqueness: true
 end
